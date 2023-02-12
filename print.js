a7f3832f7a816f2d7edacf84e42e4802e877c31f
kaibalya jena <kaibalya18j@gmail.com>
2023-02-12 08:56:36 +0530

    print syntax

---
+++ print.js
@@ -0,0 +1,4 @@
+console.log("this is the print syntax"); //double quotes
+console.log('this is the print syntax2');  //single quotes
+console.log(`this is the print syntax3`);   //babtics
+console.log("this is the print syntax") //will also work without semicolon
