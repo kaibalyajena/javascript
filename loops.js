a13d50664e98d245bc590abd5f6f558cb4c9406e
kaibalya jena <kaibalya18j@gmail.com>
2023-02-13 09:47:24 +0530

    loops

---
+++ loops.js
@@ -0,0 +1,45 @@
+// if () {
+
+// } else if () {
+
+// } else {
+
+// }
+
+
+//switch case
+let day = 4;
+switch (day) {
+    case 0:
+        console.log("sunday");
+        break;
+    case 1:
+        console.log("MONDAY");
+        break;
+    case 2:
+        console.log("TUESDAY");
+        break;
+    default:
+        console.log("invalid");
+}
+
+
+//while loop
+let i = 0;
+while (i <= 3) {
+    console.log(i);
+    i++;
+}
+
+//for loop
+for (let i = 0; i < n; i++) {
+
+}
+
+// break;
+// continue;
+
+//do while
+// do {
+
+// } while ();
